f7e3c21c50eaac0ed2d066f41a0dde6ad6f44e5f
Dimitra Gkogkou <a5263@nb-hp1ds64.basisit.de>
2026-03-10 18:18:46 +0100

    linting

--- src/nomad_tfsc_general/schema_packages/file_parser/mppt_parser.py
+++ src/nomad_tfsc_general/schema_packages/file_parser/mppt_parser.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+
+from io import StringIO
+
+import numpy as np
+import pandas as pd
+from baseclasses.helper.utilities import convert_datetime
+
 """
 Created on Thur Feb  19 10:00:00 2026
 
@@ -12,41 +19,37 @@ MPPT measurement of the best pixel as per the following protocol:
 3.MPPT for the best pixel: starting voltage from the second IV-sweep
 4.Measure IV-sweep for the best pixel 3rd time
 """
-from io import StringIO
 
-import numpy as np
-import pandas as pd
-
-from baseclasses.helper.utilities import convert_datetime
-
-#i want to get timestamp from vtt's filename, remember to pass from read_mppt_file function
 
 def get_value(val):
     try:
         return float(val)
     except Exception:
         return None
-    
+
+
 def find_step_size(df_curve):
     for i in range(5):
-        dV= abs(df_curve.iloc[i,1] - df_curve.iloc[i+1,1])
-        if dV!= 0:
+        dV = abs(df_curve.iloc[i, 1] - df_curve.iloc[i + 1, 1])
+        if dV != 0:
             return dV
-    return ValueError("No non-zero voltage step found in the first 5 rows")
+    return ValueError('No non-zero voltage step found in the first 5 rows')
+
 
 def read_mppt_data_location_1(filedata, filename=None):
     date = filedata.split('\t')[0].split('\n')[0].split(' ')[-2]
     time = filedata.split('\t')[0].split('\n')[0].split(' ')[-1]
-    
+
     df_curve = pd.read_csv(
         StringIO(filedata),
-          sep='\t',
-          header=None,
-          skiprows=1,
-          names=['Time (s)', 'Current density (mA/cm2)', 'Voltage (V)', 'Power (mW/cm2)'])
-     
-    # changing column order for consistency and for find_step_size function 
-    df_curve= df_curve[['Time (s)', 'Voltage (V)', 'Current density (mA/cm2)',  'Power (mW/cm2)']]
+        sep='\t',
+        header=None,
+        skiprows=1,
+        names=['Time (s)', 'Current density (mA/cm2)', 'Voltage (V)', 'Power (mW/cm2)'],
+    )
+
+    # changing column order for consistency and for find_step_size function
+    df_curve = df_curve[['Time (s)', 'Voltage (V)', 'Current density (mA/cm2)', 'Power (mW/cm2)']]
 
     mppt_dict = {}
 
@@ -57,29 +60,23 @@ def read_mppt_data_location_1(filedata, filename=None):
     mppt_dict['voltage_data'] = np.array(df_curve['Voltage (V)'], dtype=np.float64)
     mppt_dict['current_density_data'] = np.array(df_curve['Current density (mA/cm2)'], dtype=np.float64)
     mppt_dict['power_data'] = np.array(df_curve['Power (mW/cm2)'], dtype=np.float64)
-    
+
     return mppt_dict
 
-def read_mppt_data_location_2(filedata, filename=None):            
-    df_header = pd.read_csv(
-        StringIO(filedata),
-        nrows=1, 
-        sep=':\t', 
-        encoding='unicode_escape', 
-        engine='python',
-        )
-        
+
+def read_mppt_data_location_2(filedata, filename=None):
     df_curve = pd.read_csv(
         StringIO(filedata),
         sep='\t',
         encoding='unicode_escape',
         engine='python',
-        )
-    
+    )
+
     df_curve = df_curve.dropna(how='any', axis=0)
 
     mppt_dict = {}
 
+    # getting the timestamp from the file's name
     if filename:
         date = filename.split('_')[-2]
         time = filename.split('_')[-1].split('.')[0]
@@ -94,10 +91,11 @@ def read_mppt_data_location_2(filedata, filename=None):
 
     return mppt_dict
 
+
 def read_mppt_file(filedata, filename):
-        if 'MPP' in filename.split('.')[-1]:
-            return read_mppt_data_location_1(filedata, filename)
-        elif filedata.strip().split('\t')[0] == 'Time (s)':
-            return read_mppt_data_location_2(filedata, filename)
-        else:
-            raise TypeError('mppt file not recognized')
+    if 'MPP' in filename.split('.')[-1]:
+        return read_mppt_data_location_1(filedata, filename)
+    elif filedata.strip().split('\t')[0] == 'Time (s)':
+        return read_mppt_data_location_2(filedata, filename)
+    else:
+        raise TypeError('mppt file not recognized')
--- tests/parsers/test_mppt.py
+++ tests/parsers/test_mppt.py
@@ -1,8 +1,6 @@
-from nomad.client import normalize_all, parse
+from nomad.client import normalize_all
 from nomad.units import ureg
 from utils import delete_json, get_archive
-import os
-import json
 
 
 def test_mppt_parser_loc_1(monkeypatch):
@@ -13,12 +11,12 @@ def test_mppt_parser_loc_1(monkeypatch):
     assert archive.data
 
     assert archive.data.datetime.isoformat() == '2025-07-23T15:20:24+00:00'
-    # Check that curves are parsed 
-    assert len(archive.data.time)== 39
+    # Check that curves are parsed
+    assert len(archive.data.time) == 39
     assert len(archive.data.time) == len(archive.data.power_density)
     assert len(archive.data.voltage) == len(archive.data.current_density)
     # Check specific curve values
-    assert round(archive.data.time[-1],5) == 121.48237 * ureg('s')
+    assert round(archive.data.time[-1], 5) == 121.48237 * ureg('s')
     assert round(archive.data.power_density[5], 5) == 11.26418 * ureg('mW/cm^2')
     assert round(archive.data.current_density[5], 5) == 15.86504 * ureg('mA/cm^2')
     assert round(archive.data.voltage[-1], 5) == 0.68000 * ureg('volt')
@@ -26,27 +24,24 @@ def test_mppt_parser_loc_1(monkeypatch):
     delete_json()
 
 
-
-
-
 def test_mppt_parser_loc_2_debug(monkeypatch):
     file = 'PERS_loc2_mppt_20260204_093607.mpp.txt'
-    
+
     archive = get_archive(file, monkeypatch)
     normalize_all(archive)
 
     assert archive.data
 
     assert archive.data.datetime.isoformat() == '2026-02-04T09:36:07+00:00'
-    # Check that curves are parsed 
-    assert len(archive.data.time)== 103
+    # Check that curves are parsed
+    assert len(archive.data.time) == 103
     assert len(archive.data.time) == len(archive.data.power_density)
     assert len(archive.data.voltage) == len(archive.data.current_density)
     # Check specific curve values
-    assert round(archive.data.time[-1],5) == 204.0 * ureg('s')
+    assert round(archive.data.time[-1], 5) == 204.0 * ureg('s')
     assert round(archive.data.power_density[5], 5) == -13.51071 * ureg('mW/cm^2')
     assert round(archive.data.current_density[5], 5) == -15.71013 * ureg('mA/cm^2')
     assert round(archive.data.voltage[-1], 5) == 0.85 * ureg('volt')
     assert round(archive.data.properties.perturbation_voltage, 2) == 0.01 * ureg('volt')
-    
+
     delete_json()
